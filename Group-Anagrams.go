bd7b3e636ea58e595eeaeadea8d32c6c506be60c
mbakhouc <bakhouch2019@gmail.com>
2025-02-03 10:27:04 +0100

    groupAnagrams

---
+++ Group-Anagrams.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(groupAnagrams([]string{"eat","tea","tan","ate","nat","bat"}))
+}
+
+func groupAnagrams(strs []string)[][]string {
+	var fianlAtt [][]string
+	var myMap = make(map[string][]string)
+	var s string
+	var bigS string
+	var n = 0
+	for _, v := range strs {
+		for i := 'a'; i <= 'z'; i++ {
+			for _, char := range v {
+				if char == i {
+					n++
+				}
+			}
+			if n != 0 {
+				s = fmt.Sprintf("%c%d", i, n)
+				bigS += s
+			}
+			s = ""
+			n = 0
+		}
+		myMap[bigS] = append(myMap[bigS], v)
+		bigS = ""
+	}
+	for _, arr := range myMap {
+		fianlAtt = append(fianlAtt, arr)
+	}
+	return fianlAtt
+}
